ad2cfff7ecf8115f9a2837472f1793615bbeb1f2
DobienC <dobien_c@yahoo.com>
2019-04-29 00:36:05 -0700

    Note input section added

--- public/app.js
+++ public/app.js
@@ -10,14 +10,48 @@ $.getJSON("/articles", function(data) {
             + data[i].title + "<br />" + 
             "<a href='" + data[i].link + "' target='_blank'>" + data[i].link +"</a>" + 
             "</p></div>" +
-            "<button class='addNote btn-primary' value='" + data[i]._id + "'>Add Note</button>" +
+            "<button class='createNote btn-primary' value='" + data[i]._id + "'>Create Note</button>" +
+            "<button class='getNotes btn-secondary' value='"+ data[i]._id + "'>Get Notes</button>" +
             "</div>");
     }
     // value='" + data[i]._id + "'
 });
 
 
+$(document).on("click", ".createNote", function(){
+    var title;
+    var value = $(this).val();
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + $(this).val()
+    })
+    .then(function(response){
+        title = response.title;
+        $(".newNote").empty();
+        $(".newNote").html(
+            '<div class="form-group"><label for="comment">' +
+            title + '</label>' +
+            '<textarea class="form-control" row="8" id="newNote_' +
+            value + '"></textarea>' +
+            '<button class="addNote" value="'+ value +'">Add Note</button></div>'
+        );
+    });
+});
+
 $(document).on("click", ".addNote", function(){
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + $(this).val(),
+        data: {
+            body: $("#newNote_" + $(this).val()).val()
+        }
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+});
+
+$(document).on("click", ".getNotes", function(){
     $.ajax({
         method: "GET",
         url: "/articles/" + $(this).val()
@@ -25,4 +59,8 @@ $(document).on("click", ".addNote", function(){
     .then(function(response){
         console.log(response);
     });
-});
+})
+{/* <div class="form-group">
+    <label for="comment">Comment:</label>
+    <textarea class="form-control" rows="5" id="comment"></textarea>
+</div> */}
